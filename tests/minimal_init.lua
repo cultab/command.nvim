7260b9a65374035209424d4e929a2e4a884914fe
cultab <rroarck@gmail.com>
2024-04-05 16:06:19 +0300

    tests: add actual tests

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,11 +1,15 @@
-local plenary_dir = os.getenv("PLENARY_DIR") or "/tmp/plenary.nvim"
-local is_not_a_directory = vim.fn.isdirectory(plenary_dir) == 0
+local plugin_dir = os.getenv("PLUGIN_DIR") or "/tmp/plugins"
+local is_not_a_directory = vim.fn.isdirectory(plugin_dir) == 0
 if is_not_a_directory then
-  vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plenary_dir })
+  vim.fn.system({ "git", "clone", "https://github.com/nvim-lua/plenary.nvim", plugin_dir .. '/plenary.nvim' })
+  vim.fn.system({ "git", "clone", "https://github.com/akinsho/toggleterm.nvim", plugin_dir .. '/toggleterm.nvim' })
+  vim.fn.system({ "git", "clone", "https://github.com/MunifTanjim/nui.nvim", plugin_dir .. '/nui.nvim' })
 end
 
 vim.opt.rtp:append(".")
-vim.opt.rtp:append(plenary_dir)
+vim.opt.rtp:append(plugin_dir .. '/plenary.nvim')
+vim.opt.rtp:append(plugin_dir .. '/toggleterm.nvim')
+vim.opt.rtp:append(plugin_dir .. '/nui.nvim')
 
 vim.cmd("runtime plugin/plenary.vim")
 require("plenary.busted")
