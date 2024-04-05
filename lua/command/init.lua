51cc75aa817b4656b9ad1d72561da32430d55d21
cultab <rroarck@gmail.com>
2024-04-05 15:11:10 +0300

    chore(stylua): format again

--- lua/command/init.lua
+++ lua/command/init.lua
@@ -1,5 +1,5 @@
 local M = {}
-local notify = require'command.utils'.notify
+local notify = require('command.utils').notify
 
 M.LastCommand = nil
 M.CommandDirection = 1
@@ -11,9 +11,9 @@ M.opts = {}
 
 --- @type backend[]
 local backends = {
-	wezterm = require'command.wezterm',
-	tmux = require'command.tmux',
-	toggleterm = require'command.toggleterm',
+	wezterm = require 'command.wezterm',
+	tmux = require 'command.tmux',
+	toggleterm = require 'command.toggleterm',
 }
 
 --- @alias rule table<string, fun(string?):string>
@@ -28,28 +28,28 @@ local backends = {
 
 --- @type opts
 local default_opts = {
-	use = "auto",
+	use = 'auto',
 	rules = {
-		[".*%.lua"] = function(filepath)
-			return "nvim -l " .. filepath
+		['.*%.lua'] = function(filepath)
+			return 'nvim -l ' .. filepath
 		end,
-		["Makefile"] = function(_)
-			return "make"
+		['Makefile'] = function(_)
+			return 'make'
 		end,
 	},
 	validate = true,
-	icon = "$ ",
+	icon = '$ ',
 }
 
 local valid_opts = {
-	"use",
-	"rules",
-	"validate",
-	"icon"
+	'use',
+	'rules',
+	'validate',
+	'icon',
 }
 
 ---@param user_opts opts
-M.setup = function (user_opts)
+M.setup = function(user_opts)
 	M.opts = vim.tbl_deep_extend('force', default_opts, user_opts or {})
 
 	if M.opts.validate then
@@ -67,29 +67,29 @@ M.setup = function (user_opts)
 	end
 
 	M.popup_options = {
-		relative = "editor",
+		relative = 'editor',
 		position = '50%',
 		size = {
 			width = 24,
 		},
 		border = {
-			style = "rounded",
+			style = 'rounded',
 			text = {
-				top = M.opts.icon .. "cmd: ",
-				top_align = "left",
+				top = M.opts.icon .. 'cmd: ',
+				top_align = 'left',
 			},
 		},
 		win_options = {
-			winhighlight = "Normal:Normal",
+			winhighlight = 'Normal:Normal',
 		},
 	}
 
-	if M.opts.use == "auto" then
+	if M.opts.use == 'auto' then
 		if vim.env.TMUX then
 			M.backend = backends.tmux
 		elseif vim.env.WEZTERM_EXECUTABLE then
 			M.backend = backends.wezterm
-		elseif require('toggleterm') then
+		elseif require 'toggleterm' then
 			M.backend = backends.toggleterm
 		end
 	else
@@ -97,26 +97,26 @@ M.setup = function (user_opts)
 	end
 
 	if not M.backend then
-		notify("No such backend: " .. M.opts.use, 'error')
+		notify('No such backend: ' .. M.opts.use, 'error')
 	end
 end
 
-M.change_direction = function ()
+M.change_direction = function()
 	if not M.backend.directions then
-		notify("Changing directions is not supported using backend: " .. M.opts.use, 'error')
+		notify('Changing directions is not supported using backend: ' .. M.opts.use, 'error')
 		return
 	end
 	M.CommandDirection = (M.CommandDirection % #M.backend.directions + 1)
-	notify("Changed command direction to ".. M.backend.directions[M.CommandDirection].name, 'info')
+	notify('Changed command direction to ' .. M.backend.directions[M.CommandDirection].name, 'info')
 end
 
-local Input = require("nui.input")
-local event = require("nui.utils.autocmd").event
+local Input = require 'nui.input'
+local event = require('nui.utils.autocmd').event
 
 M.run_command = function()
 	local input = Input(M.popup_options, {
-		prompt = "$ ",
-		default_value = "",
+		prompt = '$ ',
+		default_value = '',
 		on_close = function()
 			-- print("Input closed!")
 		end,
@@ -153,69 +153,51 @@ M.run_last_command = function()
 	if M.LastCommand then
 		M.backend.run(M.LastCommand)
 	else
-		notify("No command to repeat", 'warn')
+		notify('No command to repeat', 'warn')
 	end
 end
 
 M.run_current_file = function()
 	local command = vim.api.nvim_buf_get_name(0)
-	local filename = vim.fn.expand('%:t')
-	local filepath = vim.fn.expand('%:p')
+	local filename = vim.fn.expand '%:t'
+	local filepath = vim.fn.expand '%:p'
 
 	for pattern, callback in pairs(M.opts.rules) do
 		if string.find(filename, pattern) then
 			command = callback(filepath)
-			goto run
+			M.backend.run(command)
+			M.LastCommand = command
+			return
 		end
 	end
 
-	do -- HACK: restrict scope of local 'perms' so we can goto after it :)
-		-- if we're gonna run the file as is, check if it's executable first
-		local perms = vim.fn.getfperm(filepath)
-		if not perms:find("x") then
-			vim.ui.select({ "Yes", "No" }, {
-					prompt = M.opts.icon .. "make executable?"
-				},
-				function(choice)
-					if choice and choice:find("[Yy]") then
-						M.backend.run("chmod +x " .. filepath)
-						M.backend.run(command)
-						M.LastCommand = command
-					else
-						notify("didn't run file, as it's not executable", 'info')
-					end
-				end)
-			goto skip
-		end
+	-- if we're gonna run the file as is, check if it's executable first
+	local perms = vim.fn.getfperm(filepath)
+	if not perms:find 'x' then
+		vim.ui.select({ 'Yes', 'No' }, {
+			prompt = M.opts.icon .. 'make executable?',
+		}, function(choice)
+			if choice and choice:find '[Yy]' then
+				M.backend.run('chmod +x ' .. filepath)
+				M.backend.run(command)
+				M.LastCommand = command
+			else
+				notify("didn't run file, as it's not executable", 'info')
+			end
+		end)
+	else
+		M.backend.run(command)
+		M.LastCommand = command
 	end
-
-	::run::
-	M.backend.run(command)
-	M.LastCommand = command
-	::skip::
-
 end
 
 vim.api.nvim_create_user_command(
-	"CommandChangeDirection",
+	'CommandChangeDirection',
 	M.change_direction,
-	{desc = "Toggles pane direction for running commands"}
+	{ desc = 'Toggles pane direction for running commands' }
 )
-vim.api.nvim_create_user_command(
-	"CommandRun",
-	M.run_command,
-	{desc = "Prompt to run command"}
-)
-vim.api.nvim_create_user_command(
-	"CommandFile",
-	M.run_current_file,
-	{desc = "Run current file"}
-)
-vim.api.nvim_create_user_command(
-	"CommandLast",
-	M.run_last_command,
-	{desc = "Run last command"}
-)
-
+vim.api.nvim_create_user_command('CommandRun', M.run_command, { desc = 'Prompt to run command' })
+vim.api.nvim_create_user_command('CommandFile', M.run_current_file, { desc = 'Run current file' })
+vim.api.nvim_create_user_command('CommandLast', M.run_last_command, { desc = 'Run last command' })
 
 return M
